21262fd2463f2ba2a64dc1ccd35d679b42007d3d
Jolan <rathelotj@gmail.com>
2022-05-02 13:28:18 +0200

    namespace begins

---
+++ ft.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.hpp                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrathelo <student.42nice.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 13:23:37 by jrathelo          #+#    #+#             */
+/*   Updated: 2022/05/02 13:27:20 by jrathelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+namespace ft {
+	class vector;
+	class stack;
+	class map;
+}
+
+#include "vector.hpp"
+#include "stack.hpp"
+#include "map.hpp"
